ffef397908a3d9b9ce0a81abf5364db1a3d21356
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-05-05 10:56:35 -0300

    Add basic data router 🤞

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
-function App() {
-  return <main>Hello there</main>;
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useLoaderData,
+} from "react-router-dom";
+
+const loader = async () => {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/todos/1"
+  ).then((res) => res.json());
+  return { message: "Hello, world!", response };
+};
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    loader,
+    Component,
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
 }
 
-export default App;
+function Component() {
+  const data = useLoaderData() as Awaited<ReturnType<typeof loader>>;
+  console.log(data.response);
+
+  return <h1>{data.message}</h1>;
+}
+
+if (import.meta.hot) {
+  import.meta.hot.dispose(() => router.dispose());
+}
