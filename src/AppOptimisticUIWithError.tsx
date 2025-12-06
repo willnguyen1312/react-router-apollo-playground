56b6b662e96ecf0e60d74da8502d7394c631fac8
Nam Nguyen <nam.nguyen.node@gmail.com>
2025-12-06 11:25:51 -0700

    Revert in case it fails 🚆

---
+++ src/AppOptimisticUIWithError.tsx
@@ -0,0 +1,127 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  useLoaderData,
+  ActionFunction,
+  useFetcher,
+} from "react-router-dom";
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const db = {
+  list: [{ id: 1, name: "Item 1", isFavorite: false }],
+};
+
+const loader = async () => {
+  return {
+    data: {
+      list: db.list,
+    },
+  };
+};
+
+const action: ActionFunction = async ({ request }) => {
+  const formData = await request.json();
+  // db.list = db.list.map((item) => {
+  //   if (item.id === formData.id) {
+  //     return { ...item, isFavorite: !item.isFavorite };
+  //   }
+  //   return item;
+  // });
+
+  await sleep(1000);
+
+  return {
+    data: {
+      list: db.list,
+    },
+  };
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+
+    id: "root",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        loader,
+        action,
+        Component: Home,
+      },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
+
+function Root() {
+  return (
+    <main>
+      <Outlet />
+    </main>
+  );
+}
+
+function Home() {
+  const { data } = useLoaderData();
+
+  return (
+    <div>
+      <h1>Home</h1>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "12px",
+        }}
+      >
+        {data.list.map((item: any) => (
+          <ItemRenderer key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const ItemRenderer = ({ item }: { item: any }) => {
+  const fetcher = useFetcher({ key: `item-${item.id}` });
+  const isFavorite = fetcher.json
+    ? (fetcher.json as any).id === item.id && (fetcher.json as any).isFavorite
+    : item.isFavorite;
+
+  console.log("isFavorite:", isFavorite);
+
+  return (
+    <div key={item.id}>
+      <span>{item.name}</span>
+      <button
+        onClick={() => {
+          fetcher.submit(
+            {
+              intent: "toggle-favorite",
+              id: item.id,
+              isFavorite: !isFavorite,
+            },
+            {
+              method: "post",
+              encType: "application/json",
+            }
+          );
+        }}
+      >
+        {isFavorite ? "Unfavorite" : "Favorite"}
+      </button>
+    </div>
+  );
+};
+
+if (import.meta.hot) {
+  import.meta.hot.dispose(() => router.dispose());
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import ReactDOM from "react-dom/client";
 // import App from "./App4.tsx";
 // import App from "./SubRouter.tsx";
 // import App from "./AppPromises.tsx";
-import App from "./AppOptimisticUI.tsx";
+// import App from "./AppOptimisticUI.tsx";
+import App from "./AppOptimisticUIWithError.tsx";
 
 // worker.start().then(() => {
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
